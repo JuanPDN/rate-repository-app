88de76be0c2d2ccfe78cc3595d8e3779611edea9
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-08-16 18:14:31 -0500

    creacion del componente raiz

---
+++ src/components/Main.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, View } from "react-native";
+import RespositoryList from './RepositoryList';
+import AppBar from './AppBar';
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#e1e4e8',
+        flexGrow: 1,
+        flexShrink: 1,
+    }
+})
+
+const Main = () => {
+    return (
+        <View style={styles.container}>
+        <AppBar/>
+        <RespositoryList />
+        </View>
+    )
+}
+
+export default Main
