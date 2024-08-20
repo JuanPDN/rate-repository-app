3b3207b85d1cabd272986e48c8a61a8cb6d819da
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-08-20 17:52:51 -0500

    create component SigIn

---
+++ app/SigIn.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Text from "../src/components/Text";
+
+const SigIn = () => {
+    return (
+        <Text>The SigIn in view</Text>
+    )
+}
+
+export default SigIn
