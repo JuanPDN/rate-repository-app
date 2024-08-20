f44a3abc61d4ef5c1b6ecf335c8a0c1cce72ac36
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-08-20 17:52:07 -0500

    add expo router

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,24 +1,36 @@
 import React from "react";
 import Constants from 'expo-constants'
-import { StyleSheet, View, TouchableWithoutFeedback } from "react-native";
+import { StyleSheet, View, TouchableWithoutFeedback, ScrollView } from "react-native";
 
 import Text from './Text'
+import { Link } from "expo-router";
 
 const styles =  StyleSheet.create({
     conatainer: {
         paddingTop: Constants.statusBarHeight,
         paddingBottom: 20,
-        paddingLeft: 15,
+        paddingLeft: 10,
+        paddingRight: 10,
         backgroundColor: '#24292e',
+    },link: {
+        color: 'white',
+        fontWeight: 'bold',
+        margin: 10
     }
 })
 
 const AppBar = () => {
     return (
         <View style={styles.conatainer}>
-            <TouchableWithoutFeedback >
-            <Text fontWeight={'bold'} style={{color: 'white'}}>Repositories</Text>
-            </TouchableWithoutFeedback>
+            <ScrollView horizontal >
+                <TouchableWithoutFeedback >
+                    <Link href={'/'} fontWeight={'bold'} style={styles.link}>Repositories</Link>
+                </TouchableWithoutFeedback>
+
+                <TouchableWithoutFeedback >
+                    <Link href={'/SigIn'} fontWeight={'bold'} style={styles.link}>Sig In</Link>
+                </TouchableWithoutFeedback>
+            </ScrollView>
         </View>
     )
 }
