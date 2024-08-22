27c7cff09374274c374f403805cef16ed3e5e171
Juan Pablo Delgado <jpdelgado9641@gmail.com>
2024-08-22 15:21:51 -0500

    feat(route): add link to redirect

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { View, Image, StyleSheet } from "react-native";
+import { View, Image, StyleSheet, Pressable } from "react-native";
+import { Link } from "expo-router";
 
 import Text from "./Text";
 import Stat from "./Stat";
@@ -41,24 +42,33 @@ const RepositoryItem = ({ item }) => {
 
   return (
     <View style={styles.card}>
-      <View style={styles.containerInfo}>
-        <Image source={{ uri: item.owner.avatar_url }} style={styles.image} />
-        <View style={styles.info}>
-          <Text fontWeight={"bold"} fontSize={"subheading"}>
-            {item.full_name}
-          </Text>
-          <Text color={"textSecundary"}>{item.description}</Text>
-          <View style={styles.tag}>
-            <Text color={"white"}>{item.language}</Text>
+      <Link href={`/repository/${item?.id}`} asChild>
+        <Pressable>
+          <View style={styles.containerInfo}>
+            <Image
+              source={{ uri: item?.owner.avatar_url }}
+              style={styles.image}
+            />
+            <View style={styles.info}>
+              <Text fontWeight={"bold"} fontSize={"subheading"}>
+                {item?.full_name}
+              </Text>
+              <Text numberOfLines={3} color={"textSecundary"}>
+                {item?.description}
+              </Text>
+              <View style={styles.tag}>
+                <Text color={"white"}>{item?.language}</Text>
+              </View>
+            </View>
           </View>
-        </View>
-      </View>
-      <View style={styles.stats}>
-        <Stat label={"Stars"} value={item.stargazers_count} />
-        <Stat label={"Forks"} value={item.forks_count} />
-        <Stat label={"Reviews"} value={item.watchers_count} />
-        <Stat label={"Rating"} value={item.rating_average || 0} />
-      </View>
+          <View style={styles.stats}>
+            <Stat label={"Stars"} value={item?.stargazers_count} />
+            <Stat label={"Forks"} value={item?.forks_count} />
+            <Stat label={"Reviews"} value={item?.watchers_count} />
+            <Stat label={"Rating"} value={item?.rating_average || 0} />
+          </View>
+        </Pressable>
+      </Link>
     </View>
   );
 };
